5128e0e715307153104d2ef3cbd79901805df281
Jaspreet Singh <sidhujaspreet963@gmail.com>
2017-10-05 13:09:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var socket = require('socket.io');
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+	console.log('Listening to port 4000.');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+	console.log('Socket connection established - ', socket.id);
+
+	socket.on('chat', function(data){
+		io.sockets.emit('chat', data);
+	})
+
+	socket.on('typing', function(data){
+		socket.broadcast.emit('typing', data);
+	})
+}); 
